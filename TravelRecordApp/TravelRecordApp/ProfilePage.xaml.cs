ca2387db5773151fca59b72a449bda35b4fc1b25
figen <figengungor1@gmail.com>
2018-06-13 18:21:49 +0300

    Add ProfilePage

--- TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/ProfilePage.xaml.cs
@@ -1,9 +1,10 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TravelRecordApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -16,5 +17,42 @@ namespace TravelRecordApp
 		{
 			InitializeComponent ();
 		}
-	}
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+                using (SQLiteConnection conn = new SQLiteConnection(App.DatabaseLocation))
+                {
+
+                    conn.CreateTable<Post>();
+
+                    var postTable = conn.Table<Post>();
+
+                    var posts = postTable.ToList(); //get TableQuery as list with Linq
+                    
+                    //Get list and query from this list similar to sqlite with Linq
+                    var categories = (from p in posts
+                                      orderby p.CategoryId
+                                      select p.CategoryName).Distinct().ToList();
+
+                    Dictionary<string, int> categoriesCount = new Dictionary<string, int>();
+
+                    foreach (var category in categories)
+                    {
+                        var count = (from post in postTable
+                                     where post.CategoryName == category
+                                     select post).ToList().Count;
+
+                        categoriesCount.Add(category, count);
+                    }
+
+                    categoriesListView.ItemsSource = categoriesCount;
+
+                    postCountLabel.Text = posts.Count.ToString();
+
+                }
+
+        }
+    }
 }
