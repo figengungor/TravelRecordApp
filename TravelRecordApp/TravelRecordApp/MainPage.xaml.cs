43a0751cec4f39df0a8265e35fa18b6e999c09ea
figen <figengungor1@gmail.com>
2018-06-20 18:04:24 +0300

    Add Command Parameters

--- TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
+++ TravelRecordApp/TravelRecordApp/MainPage.xaml.cs
@@ -4,33 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TravelRecordApp.Model;
+using TravelRecordApp.ViewModel;
 using Xamarin.Forms;
 
 namespace TravelRecordApp
 {
 	public partial class MainPage : ContentPage
 	{
+        MainVM viewModel; 
+
 		public MainPage()
 		{
 			InitializeComponent();
 
             var assembly = typeof(MainPage);
 
+            viewModel = new MainVM();
+            BindingContext = viewModel;
+
             iconImage.Source = ImageSource.FromResource("TravelRecordApp.Assets.Images.plane.png", assembly);
 
 		}
-
-        private async void LoginButton_Clicked(object sender, EventArgs e)
-        {
-
-            bool canLogin = await User.Login(EmailEntry.Text, PasswordEntry.Text);
-
-            if (canLogin)
-                await Navigation.PushAsync(new HomePage());
-            else
-                await DisplayAlert("Error", "Try again", "Ok");
-         
-        }
 
         private void RegisterUserButton_Clicked(object sender, EventArgs e)
         {
---
+++ TravelRecordApp/TravelRecordApp/ViewModel/MainVM.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using TravelRecordApp.Model;
+using TravelRecordApp.ViewModel.Commands;
+
+namespace TravelRecordApp.ViewModel
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+
+        public LoginCommand LoginCommand { get; set; }
+
+        private User user;
+
+        public User User
+        {
+            get { return user; }
+            set {
+                user = value;
+                OnPropertyChanged("User");
+            }
+        }
+
+
+        private string email;
+
+        public string Email
+        {
+            get { return email; }
+            set {
+                email = value;
+                User = new User()
+                {
+                    Email = this.Email,
+                    Password = this.Password
+                };
+                OnPropertyChanged("Email");
+            }
+        }
+
+        private string password;
+
+        public string Password
+        {
+            get { return password; }
+            set {
+                password = value;
+                User = new User()
+                {
+                    Email = this.Email,
+                    Password = this.Password
+                };
+                OnPropertyChanged("Password");
+            }
+        }
+
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName) {
+            if(PropertyChanged!=null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        
+        public MainVM()
+        {
+            User = new User();
+            LoginCommand = new LoginCommand(this);
+        }
+
+        public async void Login()
+        {
+            bool canLogin = await User.Login(User.Email, User.Password);
+
+            if (canLogin)
+                await App.Current.MainPage.Navigation.PushAsync(new HomePage());
+            else
+                await App.Current.MainPage.DisplayAlert("Error", "Try again", "Ok");
+        }
+    }
+}
